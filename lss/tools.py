fixed bug in average bias tools function

lss/tools.py
@@ -17,7 +17,7 @@ def compute_average_biases(pattern):
     Compute the average bias for a pattern specifiying a set of files
     """
     files = glob(pattern)
-    return np.mean([tools.extract_bias(f)[0] for f in files])
+    return np.mean([extract_bias(f)[0] for f in files])
 
 #-------------------------------------------------------------------------------
 def add_power_labels(ax, output_units, data_type, mu_avg=False, 