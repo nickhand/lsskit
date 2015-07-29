05dd38c2ad2cb7781b868e84c2b5fc686ff00a59
Nick Hand <nhand@berkeley.edu>
2015-07-29 10:58:07 -0700

    updated with model and output working

--- lsskit/speckmod/__main__.py
+++ lsskit/speckmod/__main__.py
@@ -8,10 +8,14 @@
 import fitit
 from .. import numpy as np
 from . import plugins
-import argparse 
+
+import argparse
+import os
 
 def perform_fit():
-    
+    """
+    Fit a model to power spectrum data, installed as an entry point
+    """
     # parse the input arguments
     parser = fitit.arg_parser.initialize_parser()
     parser.formatter_class = argparse.RawTextHelpFormatter
@@ -22,82 +26,66 @@ def perform_fit():
                             help=h+plugins.ModelInput.format_help('data'))
     
     # the input model  
-    # h = "the input model, specified as:\n\n"
-    # parser.add_argument('--model', required=True, type=plugins.ModelInput.parse,
-    #                        help=h+plugins.ModelInput.format_help('model'))
+    h = "the input model, specified as:\n\n"
+    parser.add_argument('--model', required=True, type=plugins.ModelInput.parse,
+                           help=h+plugins.ModelInput.format_help('model'))
+                           
     
-    # parse
+    # the outputs
+    h = "any output storages, specified as:\n\n"
+    parser.add_argument('--storage', '-X', action='append', 
+                            type=plugins.ModelResultsStorage.parse,
+                            help=h+plugins.ModelResultsStorage.format_help('output'))
+    
+    # parse the arguments
     args = fitit.arg_parser.parse_command_line(parser.parse_args())
     if args['subparser_name'] not in ['chisq', 'mcmc']:
         raise NotImplementedError("Only `chisq` and `mcmc` subparsers allowed; not %s" %args['subparser_name'])
     args['no_file_logger'] = True
     
-    for i, j, k in args['data']:
-        print i, j, k
-    
-    #
-    # # get the parameters
-    # params, logfile = fitit.run.initialize_params(args)
+    # get the parameters
+    params, logfile = fitit.run.initialize_params(args)
 
+    # get a few needed keys
+    data = params['driver']['data'].value
+    model = params['driver']['model'].value
+    outputs = params['driver']['storage'].value
+    mode = params['driver']['mode'].value
     
-    # # get the theory and model
-    # model, theory_params = fitit.get_theory(params['theory'])
-    #
-    # # a few variables we need
-    # kmin = params['driver']['kmin'].value
-    # kmax = params['driver']['kmax'].value
-    # mode = params['driver']['mode'].value
-    # lam_type = params['driver']['type'].value
-    #
-    # # loop over each set of simulations to fit
-    # sim_name = params['driver']['sim'].value
-    # print "Fitting %s simulations..." %sim_name
-    #
-    # # get the sim data
-    # data = get_sim_data(sim_name)
-    #
-    # # get P_mm data
-    # d_mm = data['mm']
-    #
-    # # do all mass bins
-    # bin_num = params['driver']['bin_num'].value
-    # if bin_num is None:
-    #     bin_nums = range(len(data))
-    # else:
-    #     bin_nums = [bin_num]
-    # for bin_num in bin_nums:
-    #
-    #     # halo-matter and halo-halo
-    #     d_hm = data['hm%d' %bin_num]
-    #     d_hh = data['hh%d' %bin_num]
-    #
-    #     # get the lambda, enforcing the k_min and k_max
-    #     data_df = get_lambda(lam_type, d_mm, d_hm, d_hh, d_hm.bias, k_min=kmin, k_max=kmax)
-    #
-    #     print "Mass Bin #%d, b1 = %.2f" %(bin_num, d_hm.bias)
-    #     print "___________________"
-    #
-    #     # do the fit
-    #     kwargs = {}
-    #     if mode == 'chisq':
-    #         result = fitit.driver.chisq_run(params['driver'], data_df, theory_params, model.copy())
-    #     elif mode == 'mcmc':
-    #         result = fitit.driver.mcmc_run(params['driver'], data_df, theory_params, model.copy(), pool=None)
-    #         kwargs['walkers'] = result.walkers
-    #         kwargs['iterations'] = result.iterations
-    #
-    #     if params['driver']['silent'].value:
-    #         result.summarize_fit(to_screen=True)
-    #
-    #     # save
-    #     if 'chain_number' in params['driver']:
-    #         params['driver']['chain_number'].value =  "bin%d" %(bin_num)
-    #     else:
-    #         params['driver']['chain_number'] = fitit.lmfit.Parameter(value="bin%d" %(bin_num))
-    #     output_name = fitit.iotools.create_output_file(params['driver'], mode, **kwargs)
-    #     fitit.iotools.save_pickle(result, output_name)
-    #
-    # # now let's save the params too
-    # folder = params['driver']['folder'].value
-    # fitit.iotools.save_pickle(params, os.path.join(folder, 'params.pickle'))
+    # get the theory and model
+    model, theory_params = fitit.store_extra_model_info(args['model'], params['theory'])
+
+    # loop over the data
+    for key, extra, data_df in data:
+        
+        key_str = '_'.join(map(str,key.values()))
+        print "Fitting bin %s" %(", ".join("%s=%s" %(k,str(v)) for k,v in key.iteritems()))
+        print "____________________________"
+
+        # do the fit
+        kwargs = {}
+        if mode == 'chisq':
+            result = fitit.driver.chisq_run(params['driver'], data_df, theory_params, model.copy(), **extra)
+        elif mode == 'mcmc':
+            result = fitit.driver.mcmc_run(params['driver'], data_df, theory_params, model.copy(), pool=None, **extra)
+            kwargs['walkers'] = result.walkers
+            kwargs['iterations'] = result.iterations
+        else:
+            raise NotImplementedError
+
+        # summarize the fit
+        if params['driver']['silent'].value:
+            result.summarize_fit(to_screen=True)
+
+        # save the output
+        for output in outputs:
+            output.write(data.multiindex, key, result)
+        
+    # now let's save the params too
+    folder = params['driver']['folder'].value
+    filename = os.path.join(folder, 'params.dat')
+    with open(filename, 'w') as ff:
+        for k,v in args.iteritems():
+            if isinstance(v, list): v = map(str, v)
+            ff.write("%s = %s\n" %(k, str(v)))
 
