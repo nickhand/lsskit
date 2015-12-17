334264eed11ced4a6ab21103cd42849b8274890d
Nick Hand <nhand@berkeley.edu>
2015-12-16 17:45:07 -0800

    renamed and removed pkresult/pkmuresult dependency

--- lsskit/data/TeppeiSimsPower.py
+++ lsskit/data/TeppeiSimsPower.py
@@ -2,11 +2,11 @@ from lsskit.data import PowerSpectraLoader
 from lsskit.specksis import SpectraSet, HaloSpectraSet
 from lsskit import numpy as np
 from pyRSD import data as sim_data
+from nbodykit.dataset import Power1dDataSet, Power2dDataSet
 import os
-from nbodykit import pkresult, pkmuresult
 
 def make_kedges(data):
-    
+
     k = data[:,0]
     kedges = np.empty(len(k)+1)
     dk = 0.5*np.diff(k)
@@ -23,7 +23,7 @@ def load_data(data, space, **meta):
     k = data[:,0]
     kedges = make_kedges(data)
     factor = 1./12**0.5
-    
+
     if space == 'redshift':
         Nmu = 5
         muedges = np.linspace(0, 1, Nmu+1)
@@ -32,28 +32,26 @@ def load_data(data, space, **meta):
         power = np.vstack([data[:,2*i+1] for i in range(Nmu)]).T
         error = np.vstack([data[:,2*(i+1)] for i in range(Nmu)]).T
         data_dict = {'power':power, 'error':error*factor, 'k':k, 'mu':mu}
-        return pkmuresult.PkmuResult(kedges, muedges, data_dict, **meta)
+        return Power2dDataSet([kedges, muedges], data_dict, **meta)
     elif space == 'real':
         data_dict = {'power' : data[:,-2], 'error' : data[:,-1]*factor, 'k':k}
-        return pkresult.PkResult(kedges, data_dict, **meta)
+        return Power1dDataSet(kedges, data_dict, **meta)
     else:
         raise ValueError
-        
-        
-class TeppeiSims(PowerSpectraLoader):
-    name = "TeppeiSims"
+
+
+class TeppeiSimsPower(PowerSpectraLoader):
+    name = "TeppeiSimsPower"
     z = ['0.000', '0.509', '0.989']
     mass = range(4)
-    
+
     def __init__(self, root):
-        
-        # store the root directory
         self.root = root
-      
+
     @classmethod
     def register(cls):
-        PowerSpectraLoader.store_class(cls)  
-        
+        PowerSpectraLoader.store_class(cls)
+
     #--------------------------------------------------------------------------
     # data accessors
     #--------------------------------------------------------------------------
@@ -64,30 +62,30 @@ class TeppeiSims(PowerSpectraLoader):
         try:
             return getattr(self, '_Phh_'+space)
         except AttributeError:
-            
+
             d = os.path.join(self.root, 'halo')
             mass_bins = ["00020_00060", "00060_00180", "00180_00540", "00540_01620"]
             redshifts = ['z007', 'z005', 'z004']
             Pshot = [[2336., 6494., 21882., 101112.], [2849., 9174., 41152., 314465.], [4065., 16447., 111359., np.nan]]
             meta = {'box_size' : 1600., 'N1':0, 'N2':0}
             basename = 'pkmu_chi_00_h_h_{mass}_{z}_1-3_02-13binaaQ'
-                
+
             data = np.empty((len(redshifts), len(mass_bins)), dtype=object)
             for i in range(len(redshifts)):
                 for j in range(len(mass_bins)):
                     args = {'mass':mass_bins[j], 'z':redshifts[i]}
                     meta['N1'] = meta['N2'] = meta['box_size']**3 / Pshot[i][j]
-                    
+
                     filename = os.path.join(d, basename.format(**args))
                     if os.path.exists(filename):
                         data[i,j] = load_data(np.loadtxt(filename), space, **meta)
                     else:
                         data[i,j] = np.nan
-                    
+
             Phh = SpectraSet(data, coords=[self.z, self.mass], dims=['z', 'mass'])
             setattr(self, '_Phh_'+space, Phh)
             return Phh
-            
+
     def get_Pmm(self, space='real'):
         """
         Return Pmm in the space specified, either `real` or `redshift`
@@ -100,7 +98,7 @@ class TeppeiSims(PowerSpectraLoader):
             redshifts = ['z007', 'z005', 'z004']
             meta = {'box_size' : 1600., 'N1':np.inf, 'N2':np.inf}
             basename = 'pkmu_chi_00_m_m_{z}_1-3_02-13binaaQ'
-                
+
             data = np.empty(len(redshifts), dtype=object)
             for i in range(len(redshifts)):
                 filename = os.path.join(d, basename.format(z=redshifts[i]))
@@ -108,7 +106,7 @@ class TeppeiSims(PowerSpectraLoader):
                     data[i] = load_data(np.loadtxt(filename), space, **meta)
                 else:
                     data[i] = np.nan
-                    
+
             Pmm = SpectraSet(data, coords=[self.z], dims=['z'])
             setattr(self, '_Pmm_'+space, Pmm)
             return Pmm
@@ -128,28 +126,28 @@ class TeppeiSims(PowerSpectraLoader):
             redshifts = ['z007', 'z005', 'z004']
             meta = {'box_size' : 1600., 'N1':np.inf, 'N2':np.inf}
             basename = 'pk_mr0_hr0_{mass}_{z}_linaax'
-                
+
             data = np.empty((len(redshifts), len(mass_bins)), dtype=object)
             for i in range(len(redshifts)):
                 for j in range(len(mass_bins)):
-                    args = {'mass':mass_bins[j], 'z':redshifts[i]}                    
+                    args = {'mass':mass_bins[j], 'z':redshifts[i]}
                     filename = os.path.join(d, basename.format(**args))
                     if os.path.exists(filename):
                         data[i,j] = load_data(np.loadtxt(filename), space, **meta)
                     else:
                         data[i,j] = np.nan
-                    
+
             Phm = SpectraSet(data, coords=[self.z, self.mass], dims=['z', 'mass'])
             setattr(self, '_Phm_'+space, Phm)
             return Phm
-    
+
     def get_lambda(self, kind='A', space='real'):
         """
         Return the stochasticity
         """
         if space != 'real':
             raise NotImplementedError("sorry, only real-space results exist for Lambda")
-            
+
         name = '_lambda%s_%s' %(kind, space)
         try:
             return getattr(self, name)
@@ -176,7 +174,7 @@ class TeppeiSims(PowerSpectraLoader):
         try:
             return getattr(self, name)
         except AttributeError:
-            
+
             redshifts = ['z007', 'z005', 'z004']
             meta = {'box_size' : 1600., 'N1':np.inf, 'N2':np.inf}
             if smooth:
@@ -185,9 +183,9 @@ class TeppeiSims(PowerSpectraLoader):
             else:
                 d = os.path.join(self.root, 'dark_matter')
                 basename = 'pkmu_chi_01_m_m_{z}_1-3_02-13binaaQ'
-                
+
             data = np.empty(len(redshifts), dtype=object)
-            
+
             for i in range(len(redshifts)):
                 if not smooth:
                     filename = os.path.join(d, basename.format(z=redshifts[i]))
@@ -199,20 +197,20 @@ class TeppeiSims(PowerSpectraLoader):
                 else:
                     x = smooth_data[i]
                 kedges = make_kedges(x)
-                    
+
                 data_dict = {}
                 data_dict['k'] = x[:,0]
                 if not smooth:
                     data_dict['power'] = x[:,-2]*2*x[:,0]
                     data_dict['error'] = x[:,-1]*2*x[:,0]
                 else:
-                    data_dict['power'] = x[:,1]                
-                data[i] = pkresult.PkResult(kedges, data_dict, **meta)
+                    data_dict['power'] = x[:,1]
+                data[i] = Power1dDataSet(kedges, data_dict, **meta)
 
             toret = SpectraSet(data, coords=[self.z], dims=['z'])
             setattr(self, name, toret)
             return toret
-            
+
     def get_P11(self, mu, smooth=False):
         """
         Return the dark matter P11[mu^2] or P11[mu^4] correlator in real-space
@@ -221,11 +219,11 @@ class TeppeiSims(PowerSpectraLoader):
         name = '_P11_%s%s' %(mu, smooth)
         if mu not in ['mu2', 'mu4']:
             raise ValueError('`mu` must be one of [`mu2`, `mu4`]')
-            
+
         try:
             return getattr(self, name)
         except AttributeError:
-            
+
             redshifts = ['z007', 'z005', 'z004']
             meta = {'box_size' : 1600., 'N1':np.inf, 'N2':np.inf}
             if smooth:
@@ -238,12 +236,12 @@ class TeppeiSims(PowerSpectraLoader):
             else:
                 d = os.path.join(self.root, 'dark_matter')
                 basename = 'pkmu_chi_11_m_m_{z}_1-3_02-13binaaQ'
-                
+
             if mu == 'mu2':
                 cols = [-4, -2]
             else:
                 cols = [-3, -1]
-            
+
             data = np.empty(len(redshifts), dtype=object)
             for i in range(len(redshifts)):
                 if not smooth:
@@ -256,20 +254,20 @@ class TeppeiSims(PowerSpectraLoader):
                 else:
                     x = smooth_data[i]
                 kedges = make_kedges(x)
-                    
+
                 data_dict = {}
                 data_dict['k'] = x[:,0]
                 if not smooth:
                     data_dict['power'] = x[:,cols[0]]*x[:,0]**2
                     data_dict['error'] = x[:,cols[1]]*x[:,0]**2
                 else:
-                    data_dict['power'] = x[:,1]                
-                data[i] = pkresult.PkResult(kedges, data_dict, **meta)
-                    
+                    data_dict['power'] = x[:,1]
+                data[i] = Power1dDataSet(kedges, data_dict, **meta)
+
             toret = SpectraSet(data, coords=[self.z], dims=['z'])
             setattr(self, name, toret)
             return toret
-            
+
     def get_Pdv(self, smooth=False):
         """
         Return the dark matter Pdv cross-spectrum in real-space
@@ -279,7 +277,7 @@ class TeppeiSims(PowerSpectraLoader):
         try:
             return getattr(self, name)
         except AttributeError:
-            
+
             redshifts = ['z007', 'z005', 'z004']
             meta = {'box_size' : 1600., 'N1':np.inf, 'N2':np.inf}
             if smooth:
@@ -301,20 +299,20 @@ class TeppeiSims(PowerSpectraLoader):
                 else:
                     x = smooth_data[i]
                 kedges = make_kedges(x)
-                    
+
                 data_dict = {}
                 data_dict['k'] = x[:,0]
                 if not smooth:
                     data_dict['power'] = x[:,-2]*(-x[:,0])
                     data_dict['error'] = x[:,-1]*(x[:,0])
                 else:
-                    data_dict['power'] = x[:,1]                
-                data[i] = pkresult.PkResult(kedges, data_dict, **meta)
-                                    
+                    data_dict['power'] = x[:,1]
+                data[i] = Power1dDataSet(kedges, data_dict, **meta)
+
             toret = SpectraSet(data, coords=[self.z], dims=['z'])
             setattr(self, name, toret)
             return toret
-    
+
     #--------------------------------------------------------------------------
     # galaxy spectra
     #--------------------------------------------------------------------------
@@ -325,24 +323,24 @@ class TeppeiSims(PowerSpectraLoader):
         try:
             return getattr(self, '_Pgal_'+space)
         except AttributeError:
-            
+
             samples = ['gg', 'cc', 'cAs', 'sAsA', 'sAsB']
             meta = {'box_size' : 1600., 'N1':np.inf, 'N2':np.inf}
             basename = 'P{sample}_z_0_509'
-                
+
             data = np.empty(len(samples), dtype=object)
             for i in range(len(samples)):
-                
+
                 d = getattr(sim_data, basename.format(sample=samples[i]))()
                 if space == 'real' and d.shape[-1] == 13 or space == 'redshift':
                     data[i] = load_data(d, space, **meta)
                 else:
                     data[i] = np.nan
-                    
+
             Pgal = SpectraSet(data, coords=[samples], dims=['sample'])
             setattr(self, '_Pgal_'+space, Pgal)
             return Pgal
-            
+
     def get_Pgal_no_fog(self):
         """
         Return galaxy component spectra in redshift space, with no FOG effect
@@ -350,21 +348,21 @@ class TeppeiSims(PowerSpectraLoader):
         try:
             return getattr(self, '_Pgal_no_fog')
         except AttributeError:
-            
+
             samples = ['cAs', 'sAsA', 'sAsB']
             meta = {'box_size' : 1600., 'N1':np.inf, 'N2':np.inf}
             basename = 'P{sample}_no_fog_z_0_509'
-                
+
             data = np.empty(len(samples), dtype=object)
             for i in range(len(samples)):
-                
+
                 d = getattr(sim_data, basename.format(sample=samples[i]))()
                 data[i] = load_data(d, 'redshift', **meta)
-                    
+
             Pgal = SpectraSet(data, coords=[samples], dims=['sample'])
             setattr(self, '_Pgal_no_fog', Pgal)
             return Pgal
-            
+
     def get_galaxy_poles(self):
         """
         Return galaxy multipoles in redshift space
@@ -372,16 +370,16 @@ class TeppeiSims(PowerSpectraLoader):
         try:
             return getattr(self, '_poles')
         except AttributeError:
-            
+
             samples = ['mono', 'quad']
             meta = {'box_size' : 1600., 'N1':np.inf, 'N2':np.inf}
             basename = 'Pgg_{sample}_z_0_509'
-                
+
             data = np.empty(len(samples), dtype=object)
             for i in range(len(samples)):
                 d = getattr(sim_data, basename.format(sample=samples[i]))()
                 data[i] = load_data(d, 'real', **meta)
-       
+
             Pgal = SpectraSet(data, coords=[samples], dims=['sample'])
             setattr(self, '_poles', Pgal)
             return Pgal
@@ -399,7 +397,7 @@ class TeppeiSims(PowerSpectraLoader):
             biases = xray.DataArray(data, coords=[samples], dims=['sample'])
             setattr(self, '_gal_biases', biases)
             return biases
-            
+
     def get_gal_stats(self):
         """
         Return a dictionary holding the galaxy sample statistics, fractions, etc
