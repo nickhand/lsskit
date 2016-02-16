5fc1ec9de7dfed1b215826642e48d7dd804042b8
Nick Hand <nhand@berkeley.edu>
2016-02-16 12:45:59 -0800

    cutsky challenge mocks from nbodykit

--- lsskit/data/CutskyChallengeMocksPower.py
+++ lsskit/data/CutskyChallengeMocksPower.py
@@ -18,15 +18,73 @@ class CutskyChallengeMocksPower(PowerSpectraLoader):
     def register(cls):
         PowerSpectraLoader.store_class(cls)  
         
-    #--------------------------------------------------------------------------
-    # multipoles data
-    #--------------------------------------------------------------------------
-    def get_poles(self, scaled=True, average=False):
+        
+    def get_poles(self, tag="", scaled=True, average=False):
         """
-        Return the cutsky galaxy multipoles in redshift space
+        Return the cutsky galaxy multipoles in redshift space, as
+        measured by `nbodykit`
         """
         scaled_tag = 'scaled' if scaled else 'unscaled'
         name = '_poles_' + scaled_tag
+        if average: name += '_mean'
+        if tag: 
+            tag = '_'+tag
+            name += tag
+        
+        try:
+            return getattr(self, name)         
+        except AttributeError:
+        
+            # form the filename and load the data
+            d = os.path.join(self.root, 'nbodykit/poles')
+            basename = 'poles_cutskyN{box:d}_%s_dk005%s.dat' %(scaled_tag, tag)
+
+            # read in the data
+            loader = io.load_power
+            mapcols = {'power_0.real':'mono', 'power_2.real':'quad', 'power_4.real':'hexadec'}
+            usecols = ['k', 'mono', 'quad', 'hexadec', 'modes']
+            kwargs = {'usecols':usecols, 'mapcols':mapcols}
+            poles = SpectraSet.from_files(loader, d, basename, [self.boxes], ['box'], args=('1d',), kwargs=kwargs)
+        
+            # reindex
+            poles = self.reindex(poles, 'k_cen', self.dk, weights='modes')
+            
+            # unstack the poles
+            ells = [('mono',0), ('quad', 2), ('hexadec', 4)]
+            poles = tools.unstack_multipoles(poles, ells, 'power')
+            
+            # compute errors
+            errs = {}
+            for ell in poles['ell'].values:
+                data = []
+                for box in poles['box'].values:
+                    p = poles.sel(box=box, ell=ell).values
+                    data.append(p['power'])
+
+                errs[ell] = np.diag(np.cov(np.asarray(data).T))**0.5
+                if average:
+                    errs[ell] /= (len(self.boxes))**0.5
+            
+            # average?
+            if average:
+                poles = poles.average(axis='box')
+                
+            # add the errors
+            for key in poles.ndindex():
+                p = poles.loc[key].values
+                p['error'] = errs[key['ell']]
+        
+            setattr(self, name, poles)
+            return poles
+            
+
+    def get_florian_poles(self, scaled=True, average=False):
+        """
+        Return the cutsky galaxy multipoles in redshift space, as
+        measured by Florian's code
+        """
+        scaled_tag = 'scaled' if scaled else 'unscaled'
+        name = '_poles_florian_' + scaled_tag
         if average: name += '_mean'
         
         try:
