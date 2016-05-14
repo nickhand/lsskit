813368758b73025178a96aa09920272ef14dce18
Nick Hand <nhand@berkeley.edu>
2016-05-13 22:58:18 -0700

    more additions to compute rsdfit in batch mode

---
+++ lsskit/rsdfit/batch.py
@@ -0,0 +1,398 @@
+import logging
+import os
+import traceback
+import string
+import functools
+
+from pyRSD.rsdfit.util import rsdfit_parser
+from pyRSD.rsdfit import rsdfit, params_filename
+from mpi4py import MPI
+
+from . import lib
+
+#------------------------------------------------------------------------------
+# tools
+#------------------------------------------------------------------------------        
+def rsetattr(obj, attr, val):
+    """
+    Recursive setattr for multiple layers of attributes
+    """
+    pre, _, post = attr.rpartition('.')
+    return setattr(rgetattr(obj, pre) if pre else obj, post, val)
+    
+sentinel = object()
+def rgetattr(obj, attr, default=sentinel):
+    """
+    Recursive getattr for multiple layers of attributes
+    """
+    if default is sentinel:
+        _getattr = getattr
+    else:
+        def _getattr(obj, name):
+            return getattr(obj, name, default)
+    return functools.reduce(_getattr, [obj]+attr.split('.'))    
+    
+def split_ranks(N_ranks, N):
+    """
+    Divide the ranks into chunks, attempting to have `N` ranks
+    in each chunk. This removes the master (0) rank, such 
+    that `N_ranks - 1` ranks are available to be grouped
+    
+    Parameters
+    ----------
+    N_ranks : int
+        the total number of ranks available
+    N : int
+        the desired number of ranks per worker
+    """
+    available = list(range(1, N_ranks)) # available ranks to do work
+    total = len(available)
+    extra_ranks = total % N
+  
+    for i in range(total//N):
+        yield i, available[i*N:(i+1)*N]
+    
+    if extra_ranks and extra_ranks >= N//2:
+        remove = extra_ranks % 2 # make it an even number
+        ranks = available[-extra_ranks:]
+        if remove: ranks = ranks[:-remove]
+        if len(ranks):
+            yield i+1, ranks
+        
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    return type('Enum', (), enums)
+    
+#------------------------------------------------------------------------------
+# class to run rsdfit in batch mode
+#------------------------------------------------------------------------------
+class RSDFitBatch(object):
+    """
+    Task manager that will iterate over ``rsdfit`` computations,
+    possibly in parallel using MPI
+    """
+    logger = logging.getLogger('rsdfit-batch')
+    
+    def __init__(self, comm, 
+                       cpus_per_worker,
+                       batch_param_file, 
+                       command, 
+                       task_keys, 
+                       task_values, 
+                       log_level=logging.INFO):
+        """
+        Parameters
+        ----------
+        comm : MPI communicator
+            the global communicator that will be split and divided
+            amongs the independent workers
+        cpus_per_worker : int
+            the desired number of ranks assigned to each independent
+            worker, when iterating over the tasks in parallel
+        batch_param_file : str
+            the name of the configuration file holding the
+            parameters to update
+        command : RSDFitCommand
+            the ``rsdfit`` command instance
+        task_keys : list
+            a list of strings specifying the names of the task dimensions -- 
+            these specify the string formatting key when updating the config
+            template file for each task value
+        task_values : list
+            a list of tuples specifying the task values which will be iterated 
+            over -- each tuple should be the length of `task_keys`
+        log_level : int, optional
+            an integer specifying the logging level to use -- default
+            is the `INFO` level
+        """
+        self.logger.setLevel(log_level)
+        
+        self.cpus_per_worker = cpus_per_worker
+        self.command         = command
+        self.task_keys       = task_keys
+        self.task_values     = task_values
+        
+        # MPI setup
+        self.comm      = comm
+        self.size      = comm.size
+        self.rank      = comm.rank
+        self.pool_comm = None
+        
+        # the parser
+        self.parser = rsdfit_parser()
+        
+        # get the batch parameters
+        self.batch_params = lib.read_batch_parameters(batch_param_file)
+        
+        # initialize a special string formatter
+        self.formatter = string.Formatter()
+        self.formatter.parse = lambda l: lib.MyStringParse(self.formatter, l, self.task_keys)
+                
+    def task_kwargs(self, itask):
+        """
+        The dictionary of parameters to update for each task
+        """
+        return dict(zip(self.task_keys, self.task_values[itask]))
+    
+    def get_command(self, itask):
+        """
+        Copy and update the command for this task
+        """
+        # make a copy of command
+        command = self.command.copy()
+        
+        # task dict for this iteration
+        kwargs = self.task_kwargs(itask)
+                
+        # do the string formatting and update the command
+        for p in self.batch_params:
+            d = getattr(command, p.key)
+            v = self.formatter.format(p.value, **kwargs)
+            rsetattr(d, p.subkey, v)
+            
+        return command
+    
+    def initialize_driver(self):
+        """
+        Initialize the `RSDFitDriver` object on all ranks
+        """
+        # update with first value
+        itask = 0
+        task = self.task_values[itask]
+
+        # master will parse the args
+        this_config = None; rsdfit_args = None
+        if self.comm.rank == 0:
+                  
+            # get the command
+            command = self.get_command(itask)
+            
+            # this writes out the param file
+            with command:
+                this_config = command.param_file
+                self.logger.debug("creating temporary file: %s" %this_config)
+                rsdfit_args = command.args
+        
+        # bcast the file name to all in the worker pool
+        this_config = self.comm.bcast(this_config, root=0)
+        rsdfit_args = self.comm.bcast(rsdfit_args, root=0)
+        self.temp_config = this_config
+
+        # get the args
+        self.logger.debug("calling rsdfit with arguments: %s" %str(rsdfit_args))
+        
+        args = None
+        if self.comm.size > 1:
+            if self.comm.rank == 0:
+                args = self.parser.parse_args(rsdfit_args)
+            args = self.comm.bcast(args, root=0)
+        else:
+            args = self.parser.parse_args(rsdfit_args)
+                      
+        # load the driver for everyone but root 
+        if self.comm.rank != 0:     
+            args = vars(args)
+            mode = args.pop('subparser_name')
+            self.driver = rsdfit.RSDFitDriver(self.pool_comm, mode, **args)
+                    
+    def initialize_pool_comm(self):
+        """
+        Internal function that initializes the `MPI.Intracomm` used by the 
+        pool of workers. This will be passed to the task function and used 
+        in task computation
+        """
+        # split the ranks
+        self.pool_comm = None
+        chain_ranks = []
+        color = 0
+        total_ranks = 0
+        i = 0
+        for i, ranks in split_ranks(self.size, self.cpus_per_worker):
+            chain_ranks.append(ranks[0])
+            if self.rank in ranks: color = i+1
+            total_ranks += len(ranks)
+        
+        self.workers = i+1 # store the total number of workers
+        leftover= (self.size - 1) - total_ranks
+        if leftover and self.rank == 0:
+            args = (self.cpus_per_worker, self.size-1, leftover)
+            self.logger.warning("with `cpus_per_worker` = %d and %d available ranks, %d ranks will do no work" %args)
+            
+        # crash if we only have one process or one worker
+        if self.size <= self.workers:
+            args = (self.size, self.workers+1, self.workers)
+            raise ValueError("only have %d ranks; need at least %d to use the desired %d workers" %args)
+            
+        # ranks that will do work have a nonzero color now
+        self._valid_worker = color > 0
+        
+        # track how many tasks each worker does
+        if self._valid_worker:
+            self._completed_tasks = 0
+        
+        # split the comm between the workers
+        self.pool_comm = self.comm.Split(color, 0)
+                
+    def run_all(self):
+        """
+        Run all of the tasks
+        """    
+        # define MPI message tags
+        tags = enum('READY', 'DONE', 'EXIT', 'START')
+        status = MPI.Status()
+         
+        try:
+            # make the pool comm
+            self.initialize_pool_comm()
+    
+            # the total numbe rof tasks
+            num_tasks = len(self.task_values)
+            
+            # initialize the driver for everyone but master
+            self.initialize_driver()
+    
+            # master distributes the tasks
+            if self.rank == 0:
+        
+                # initialize
+                task_index = 0
+                closed_workers = 0
+        
+                # loop until all workers have finished with no more tasks
+                self.logger.info("master starting with %d worker(s) with %d total tasks" %(self.workers, num_tasks))
+                while closed_workers < self.workers:
+                    data = self.comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+                    source = status.Get_source()
+                    tag = status.Get_tag()
+            
+                    # worker is ready, so send it a task
+                    if tag == tags.READY:
+                        if task_index < num_tasks:
+                            self.comm.send(task_index, dest=source, tag=tags.START)
+                            self.logger.info("sending task `%s` to worker %d" %(str(self.task_values[task_index]), source))
+                            task_index += 1
+                        else:
+                            self.comm.send(None, dest=source, tag=tags.EXIT)
+                    elif tag == tags.DONE:
+                        results = data
+                        self.logger.debug("received result from worker %d" %source)
+                    elif tag == tags.EXIT:
+                        closed_workers += 1
+                        self.logger.debug("worker %d has exited, closed workers = %d" %(source, closed_workers))
+    
+            # worker processes wait and execute single jobs
+            elif self._valid_worker:
+                if self.pool_comm.rank == 0:
+                    args = (self.rank, MPI.Get_processor_name(), self.pool_comm.size)
+                    self.logger.info("pool master rank is %d on %s with %d processes available" %args)
+                while True:
+                    itask = -1
+                    tag = -1
+        
+                    # have the master rank of the pool ask for task and then broadcast
+                    if self.pool_comm.rank == 0:
+                        self.comm.send(None, dest=0, tag=tags.READY)
+                        itask = self.comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
+                        tag = status.Get_tag()
+                    itask = self.pool_comm.bcast(itask)
+                    tag = self.pool_comm.bcast(tag)
+        
+                    # do the work here
+                    if tag == tags.START:
+                        result = self.run_rsdfit(itask)
+                        self.pool_comm.Barrier() # wait for everyone
+                        if self.pool_comm.rank == 0:
+                            self.comm.send(result, dest=0, tag=tags.DONE) # done this task
+                    elif tag == tags.EXIT:
+                        break
+
+                self.pool_comm.Barrier()
+                if self.pool_comm.rank == 0:
+                    self.comm.send(None, dest=0, tag=tags.EXIT) # exiting
+        except Exception as e:
+            self.logger.error("an exception has occurred on one of the ranks...all ranks exiting")
+            self.logger.error(traceback.format_exc())
+            
+            # bit of hack that forces mpi4py to exit all ranks
+            # see https://groups.google.com/forum/embed/#!topic/mpi4py/RovYzJ8qkbc
+            os._exit(1)
+        
+        finally:
+            # free and exit
+            self.logger.debug("rank %d process finished" %self.rank)
+            self.comm.Barrier()
+            
+            if self.rank == 0:
+                self.logger.info("master is finished; terminating")
+                if self.pool_comm is not None:
+                    self.pool_comm.Free()
+          
+                if os.path.exists(self.temp_config): 
+                    self.logger.debug("removing temporary file: %s" %self.temp_config)
+                    os.remove(self.temp_config)
+    
+    def run_rsdfit(self, itask):
+        """
+        Run the algorithm once, using the parameters specified for this task
+        iteration specified by `itask`
+    
+        Parameters
+        ----------
+        itask : int
+            the integer index of this task
+        """
+        # initialization
+        update_data = False; update_model = False
+        fit_params = self.driver.algorithm.theory.fit_params
+        
+        # get the command
+        command = self.get_command(itask)
+        
+        # update the attributes of the RSDFitDriver
+        with command:
+            args = vars(self.parser.parse_args(command.args))
+            print "TASK = ", self.task_values[itask]
+            print "args = ", args
+            for k in args:
+                setattr(self.driver, k, args[k])
+            
+            # update data, driver, and theory values
+            for p in self.batch_params:
+
+                # the updated value
+                v = rgetattr(getattr(command, p.key), p.subkey)
+                            
+                if p.key == 'driver':
+                    print "setting driver param: ", p.subkey, v
+                    self.driver.algorithm.params.add(p.subkey, value=v)
+                elif p.key == 'data':
+                    print "setting data param: ", p.subkey, v
+                    self.driver.algorithm.data.params.add(p.subkey, value=v)
+                    update_data = True
+                elif p.key == 'theory':
+                    
+                    print "setting theory param: ", p.subkey, v
+                    name = p.subkey.split('.')[0]
+                    if name not in fit_params:
+                        raise ValueError("cannot update value of theory parameter '%s'; does not exist" %name)
+                    
+                    fit_params[name].value = v
+                    fit_params[name].fiducial = v
+                    update_model = True
+                        
+                
+        # re-initialize the data
+        if update_data:
+            self.driver.algorithm.data.initialize()
+            
+        # update the model values        
+        if update_model:
+            fit_params.update_values()
+            self.driver.algorithm.theory.update_model()
+        
+        # write the parameters to file
+        filename = os.path.join(self.driver.folder, params_filename)
+        self.driver.algorithm.to_file(filename)
+            
+        # okay, now run
+        self.driver.run()
--- lsskit/rsdfit/lib.py
+++ lsskit/rsdfit/lib.py
@@ -11,10 +11,137 @@ import os, sys
 import tempfile
 import subprocess
 import string
+from collections import namedtuple
+import copy
 
 from . import DriverParams, BaseTheoryParams
 from . import PkmuDataParams, PoleDataParams
 from . import RSDFIT_BIN, RSDFIT_PARAMS
+
+class RSDFitCommand(object):
+    """
+    Class to represent a ``rsdfit`` command
+    """
+    def __init__(self, config, stat, kmax, 
+                    theory_options=[], options=[], tag="", executable=None):
+        """    
+        Parameters
+        ----------
+        config : str
+            the name of the file holding the configuration parameters, from which the
+            the parameter file for ``rsdfit`` will be initialized
+        stat : str, {`pkmu`, `poles`}
+            either `pkmu` or `poles` -- the mode of the RSD fit
+        kmax : list
+            the kmax values to use
+        theory_options : list
+            list of options to apply the theory model, i.e., `mu_corr` or `so_corr`
+        options : list
+            list of additional options to pass to the ``rsdfit`` command
+        tag : str
+            the name of the tag to append to the output directory
+        executable : str
+            the executable command to call
+
+        """    
+        # initialize the driver, theory, and data parameter objects
+        self.driver = DriverParams.from_file(config, ignore=['theory', 'model', 'data'])
+        self.theory = BaseTheoryParams.from_file(config, ignore=['driver', 'data'])
+        if stat == 'pkmu':
+            self.data = PkmuDataParams.from_file(config, ignore=['theory', 'model', 'driver'])
+        else:
+            self.data = PoleDataParams.from_file(config, ignore=['theory', 'model', 'driver'])
+            
+        # apply any options to the theory model
+        if theory_options is not None:
+            self.theory.apply_options(*theory_options)
+        
+        # set the kmax
+        self.data.kmax = kmax
+        
+        # store the rest
+        self.tag = tag
+        self.executable = executable
+        self.input_options = options
+        if self.executable is None: self.executable = RSDFIT_BIN
+        
+        # initialize the components
+        self.output_dir = None
+        self.param_file = None
+        self.args       = None
+    
+    def copy(self):
+        return copy.copy(self)    
+    
+    @property
+    def initialized(self):
+        """
+        Whether the commmand has been properly initialized
+        """
+        return self.param_file is not None
+    
+    def __enter__(self):
+        """
+        Initialize the temporary file and write the parameters
+        """
+        # write out the parameters to a temporary file
+        with tempfile.NamedTemporaryFile(delete=False) as ff:
+            self.param_file = ff.name
+        
+            # write out the parameters to the specified output
+            self.driver.to_file(ff)
+            self.data.to_file(ff)
+            self.theory.to_file(ff)
+            
+        # get the output name
+        tags = [x.name for x in [self.data, self.theory, self.driver] if x.name]
+        if self.tag: tags.append(self.tag)
+        self.output_dir = os.path.join(self.driver.output, self.driver.solver_type + '_' + "_".join(tags))
+    
+        # copy the options
+        self.args = list(self.input_options)
+        
+        # now add the rest of the options
+        self.args += ['run', '-o', self.output_dir, '-p', self.param_file]
+        
+        # add the model?
+        if self.driver.model_file is not None:
+            if all(x not in self.args for x in ['-m', '--model']):
+                self.args += ['-m', self.driver.model_file]
+
+        # add extra param file?
+        if all(x not in self.args for x in ['-xp', '--extra_params']):
+            xparams = os.path.join(RSDFIT_PARAMS, 'general', 'extra.params')
+            if os.path.isfile(xparams):
+                self.args += ['-xp', xparams]
+
+        # return
+        return self
+        
+    def __exit__(self, *args):
+        """
+        Reset the command
+        """
+        # reset
+        self.output_dir = None
+        self.param_file = None
+        self.args       = None
+
+    def __str__(self):
+        """
+        The string representation of the command
+        """
+        return " ".join(self.__call__())
+    
+    def __call__(self):
+        """
+        Return the `split` command as a list
+        """
+        if not self.initialized:
+            raise ValueError("``rsdfit`` command not currently initialized")
+        
+        return self.executable.split() + self.args
+
 
 def write_rsdfit_params(mode, config, output, theory_options=[]):
     """
@@ -48,78 +175,7 @@ def write_rsdfit_params(mode, config, output, theory_options=[]):
     driver.to_file(output)
     data.to_file(output)
     theory.to_file(output)
-    
-def construct_rsdfit_call(config, stat, kmax, 
-                            theory_options=[], rsdfit_options=[], tag="", command=None):
-    """
-    Construct and return the string representation of the ``rsdfit`` command
-    
-    Parameters
-    ----------
-    config : str
-        the name of the file holding the configuration parameters, from which the
-        the parameter file for ``rsdfit`` will be initialized
-    stat : str, {`pkmu`, `poles`}
-        either `pkmu` or `poles` -- the mode of the RSD fit
-    kmax : list
-        the kmax values to use
-    theory_options : list
-        list of options to apply the theory model, i.e., `mu_corr` or `so_corr`
-    rsdfit_options : list
-        list of additional options to pass to the ``rsdfit`` command
-    tag : str
-        the name of the tag to append to the output directory
-    command : str
-        the executable command to call
-    """
-    # write out the parameters to a temporary file
-    ff = tempfile.NamedTemporaryFile(delete=False)
-    param_file = ff.name
-    
-    # initialize the driver, theory, and data parameter objects
-    driver = DriverParams.from_file(config, ignore=['theory', 'model', 'data'])
-    theory = BaseTheoryParams.from_file(config, ignore=['driver', 'data'])
-    if stat == 'pkmu':
-        data = PkmuDataParams.from_file(config, ignore=['theory', 'model', 'driver'])
-    else:
-        data = PoleDataParams.from_file(config, ignore=['theory', 'model', 'driver'])
-            
-    # apply any options to the theory model
-    if theory_options is not None:
-        theory.apply_options(*theory_options)
-        
-    # set the kmax
-    data.kmax = kmax
-
-    # write out the parameters to the specified output
-    driver.to_file(ff)
-    data.to_file(ff)
-    theory.to_file(ff)
-    ff.close()
-    
-    # get the output name
-    tags = [x.name for x in [data, theory, driver] if x.name]
-    if tag: tags.append(tag)
-    output_dir = os.path.join(driver.output, driver.solver_type + '_' + "_".join(tags))
-    
-    # add the model?
-    if driver.model_file is not None:
-        if all(x not in rsdfit_options for x in ['-m', '--model']):
-            rsdfit_options += ['-m', driver.model_file]
-
-    # add extra param file?
-    if all(x not in rsdfit_options for x in ['-xp', '--extra_params']):
-        xparams = os.path.join(RSDFIT_PARAMS, 'general', 'extra.params')
-        if os.path.isfile(xparams):
-            rsdfit_options += ['-xp', xparams]
-
-    # the executable command
-    if command is None:
-        command = RSDFIT_BIN
-        
-    # return the call signature
-    return command.split() + ['run', '-o', output_dir, '-p', param_file] + rsdfit_options
-   
+       
 def run_rsdfit(config, stat, kmax, 
                 theory_options=[], rsdfit_options=[], tag="", 
                 command=None, nodes=None, partition=None):
@@ -153,34 +209,37 @@ def run_rsdfit(config, stat, kmax,
         the executable command to call
     """
     # make the call signature
-    kws = {'theory_options':theory_options, 'rsdfit_options':rsdfit_options, 
-            'tag':tag, 'command':command}
-    call_signature = construct_rsdfit_call(config, stat, kmax, **kws)
+    kws = {}
+    kws['theory_options'] = theory_options
+    kws['options'] = rsdfit_options, 
+    kws['tag'] = tag
+    kws['executable'] = command
     
-    # run the command
-    if nodes is None and partition is None:
-        try:
-            ret = subprocess.call(call_signature)
-            if ret: raise
-        except Exception as e:
-            if not isinstance(e, KeyboardInterrupt):        
-                msg = "error calling ``rsdfit``:\n" + "-"*20 + "\n"
-                msg += "\toriginal command:\n\t\t%s\n" %(" ".join(call_signature))
-                msg += "\toriginal exception: %s\n" %e.msg
-                e.msg = msg
-            raise e
-        finally:
-            i = call_signature.index('-p')
-            param_file = call_signature[i+1]
-            if os.path.exists(param_file):
-               os.remove(param_file)
-    # submit the job
-    else:
-        if nodes is None or partition is None:
-            raise ValueError("both `nodes` and `partition` must be given to submit job")
+    # create the command
+    with RSDFitCommand(config, stat, kmax, **kws) as command:
+    
+        # run the command
+        if nodes is None and partition is None:
+            try:
+                ret = subprocess.call(command())
+                if ret: raise
+            except Exception as e:
+                if not isinstance(e, KeyboardInterrupt):        
+                    msg = "error calling ``rsdfit``:\n" + "-"*20 + "\n"
+                    msg += "\toriginal command:\n\t\t%s\n" %(command)
+                    msg += "\toriginal exception: %s\n" %e.msg
+                    e.msg = msg
+                raise e
+            finally:
+                if os.path.exists(command.param_file):
+                   os.remove(command.param_file)
         
-        command = " ".join(call_signature)
-        submit_rsdfit_job(command, nodes, partition)
+        # submit the job
+        else:
+            if nodes is None or partition is None:
+                raise ValueError("both `nodes` and `partition` must be given to submit job")
+
+            submit_rsdfit_job(str(command), nodes, partition)
 
 def submit_rsdfit_job(command, nodes, partition):
     """
@@ -270,3 +329,39 @@ def make_temp_config(config, key, value):
         ff.write(formatter.format(config, **d))
         
     return fname
+    
+def read_batch_parameters(fname):
+    """
+    Read the parameters that will be updated for each iteration in 
+    batch mode, from a template configuration file
+    
+    Parameters
+    ----------
+    fname : str
+        the name of the file to read from
+
+    Returns
+    -------
+    list of BatchParam :
+        a list of named tuples that have `key`, `subkeys`, and `value`
+        fields
+    """
+    BatchParam = namedtuple('BatchParam', ['key', 'subkey', 'value'])
+    
+    toret = []
+    lines = open(fname, 'r').readlines()
+    for line in lines:
+        
+        li = line.strip()
+        if not li.startswith("#"):
+            fields = li.split('=')
+            if len(fields) == 2:
+                
+                names = fields[0].strip().split('.')
+                subkey = '.'.join(names[1:])
+                value = eval(fields[1].strip())
+                
+                tup = BatchParam(key=names[0], subkey=subkey, value=value)
+                toret.append(tup)
+                
+    return toret
