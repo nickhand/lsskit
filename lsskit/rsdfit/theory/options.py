0e216846152b84846f2849c696626fffdbfdbd64
Nick Hand <nhand@berkeley.edu>
2016-07-20 15:26:51 -0700

    testing explicit nonlinear biasing

--- lsskit/rsdfit/theory/options.py
+++ lsskit/rsdfit/theory/options.py
@@ -16,8 +16,12 @@ def use_b2_00(params):
     ``b2_00`` as parameters
     """
     name = 'b2_00'
-    biases_params = [name + '_%s' %i for i in ['a', 'b', 'c', 'd']] + [name + '__' + str(i) for i in [0, 2, 4]]
     
+    biases_params = []
+    for ext in ['', '_a', '_b', '_c', '_d']:
+        for i in [0, 2, 4]:
+            biases_params.append(name+ext+'__'+str(i))
+      
     # make the parameters valid
     for b in biases_params:
         if b not in params.valid_params:
@@ -27,11 +31,22 @@ def use_b2_00(params):
     params.b2_00__2 = AttrDict(vary=True, fiducial=0.03, prior='normal', mu=0.03, sigma=0.1)
     params.b2_00__4 = AttrDict(vary=True, fiducial=0.05, prior='normal', mu=0.05, sigma=0.05)
         
-    params.b2_00_a = AttrDict(vary=False, expr="[b2_00__4, 0., b2_00__2, 0., b2_00__0]")
-    params.b2_00_b = AttrDict(vary=False, expr="b2_00_a")
-    params.b2_00_c = AttrDict(vary=False, expr="b2_00_a")
-    params.b2_00_d = AttrDict(vary=False, expr="b2_00_a")
+    params.b2_00_a__0 = AttrDict(vary=False, expr="b2_00__0")
+    params.b2_00_a__2 = AttrDict(vary=False, expr="b2_00__2")
+    params.b2_00_a__4 = AttrDict(vary=False, expr="b2_00__4")
     
+    params.b2_00_b__0 = AttrDict(vary=False, expr="b2_00__0")
+    params.b2_00_b__2 = AttrDict(vary=False, expr="b2_00__2")
+    params.b2_00_b__4 = AttrDict(vary=False, expr="b2_00__4")
+    
+    params.b2_00_c__0 = AttrDict(vary=False, expr="b2_00__0")
+    params.b2_00_c__2 = AttrDict(vary=False, expr="b2_00__2")
+    params.b2_00_c__4 = AttrDict(vary=False, expr="b2_00__4")
+    
+    params.b2_00_d__0 = AttrDict(vary=False, expr="b2_00__0")
+    params.b2_00_d__2 = AttrDict(vary=False, expr="b2_00__2")
+    params.b2_00_d__4 = AttrDict(vary=False, expr="b2_00__4")
+        
     params.options.append('b2_00')
 
 
@@ -41,7 +56,7 @@ def use_b2_00_a(params):
     ``b2_00_a`` as parameters
     """
     name = 'b2_00_a'
-    biases_params = [name] + [name + '__' + str(i) for i in [0, 2, 4]]
+    biases_params = [name + '__' + str(i) for i in [0, 2, 4]]
     
     # make the parameters valid
     for b in biases_params:
@@ -51,7 +66,6 @@ def use_b2_00_a(params):
     params.b2_00_a__0 = AttrDict(vary=True, fiducial=-0.91, prior='normal', mu=-0.91, sigma=0.2)
     params.b2_00_a__2 = AttrDict(vary=True, fiducial=0.42, prior='normal', mu=0.42, sigma=0.15)
     params.b2_00_a__4 = AttrDict(vary=True, fiducial=0.008, prior='normal', mu=0.008, sigma=0.05)
-    params.b2_00_a    = AttrDict(vary=False, expr="[b2_00_a__4, 0., b2_00_a__2, 0., b2_00_a__0]")
     
     params.options.append('b2_00_a')
     
@@ -61,7 +75,7 @@ def use_b2_00_b(params):
     ``b2_00_b`` as parameters
     """
     name = 'b2_00_b'
-    biases_params = [name] + [name + '__' + str(i) for i in [0, 2, 4]]
+    biases_params = [name + '__' + str(i) for i in [0, 2, 4]]
     
     # make the parameters valid
     for b in biases_params:
@@ -71,7 +85,6 @@ def use_b2_00_b(params):
     params.b2_00_b__0 = AttrDict(vary=True, fiducial=-0.63, prior='normal', mu=-0.63, sigma=0.2)
     params.b2_00_b__2 = AttrDict(vary=True, fiducial=0.32, prior='normal', mu=0.32, sigma=0.15)
     params.b2_00_b__4 = AttrDict(vary=True, fiducial=0.03, prior='normal', mu=0.03, sigma=0.05)
-    params.b2_00_b    = AttrDict(vary=False, expr="[b2_00_b__4, 0., b2_00_b__2, 0., b2_00_b__0]")
     
     params.options.append('b2_00_b')
     
@@ -81,7 +94,7 @@ def use_b2_00_c(params):
     ``b2_00_c`` as parameters
     """
     name = 'b2_00_c'
-    biases_params = [name] + [name + '__' + str(i) for i in [0, 2, 4]]
+    biases_params = [name + '__' + str(i) for i in [0, 2, 4]]
     
     # make the parameters valid
     for b in biases_params:
@@ -91,7 +104,6 @@ def use_b2_00_c(params):
     params.b2_00_c__0 = AttrDict(vary=True, fiducial=-0.6, prior='normal', mu=-0.6, sigma=0.2)
     params.b2_00_c__2 = AttrDict(vary=True, fiducial=0.03, prior='normal', mu=0.03, sigma=0.1)
     params.b2_00_c__4 = AttrDict(vary=True, fiducial=0.05, prior='normal', mu=0.05, sigma=0.05)
-    params.b2_00_c    = AttrDict(vary=False, expr="[b2_00_c__4, 0., b2_00_c__2, 0., b2_00_c__0]")
     
     params.options.append('b2_00_c')
     
@@ -101,7 +113,7 @@ def use_b2_00_d(params):
     ``b2_00_d`` as parameters
     """
     name = 'b2_00_d'
-    biases_params = [name] + [name + '__' + str(i) for i in [0, 2, 4]]
+    biases_params = [name + '__' + str(i) for i in [0, 2, 4]]
     
     # make the parameters valid
     for b in biases_params:
@@ -111,7 +123,6 @@ def use_b2_00_d(params):
     params.b2_00_d__0 = AttrDict(vary=True, fiducial=0.47, prior='normal', mu=0.47, sigma=0.2)
     params.b2_00_d__2 = AttrDict(vary=True, fiducial=-0.5, prior='normal', mu=-0.5, sigma=0.2)
     params.b2_00_d__4 = AttrDict(vary=True, fiducial=0.10, prior='normal', mu=0.10, sigma=0.05)
-    params.b2_00_d    = AttrDict(vary=False, expr="[b2_00_d__4, 0., b2_00_d__2, 0., b2_00_d__0]")
     
     params.options.append('b2_00_d')
     
@@ -121,7 +132,7 @@ def use_b2_01_a(params):
     ``b2_01_a`` as parameters
     """
     name = 'b2_01_a'
-    biases_params = [name] + [name + '__' + str(i) for i in [0, 1, 2]]
+    biases_params = [name + '__' + str(i) for i in [0, 1, 2]]
     
     # make the parameters valid
     for b in biases_params:
@@ -131,7 +142,6 @@ def use_b2_01_a(params):
     params.b2_01_a__0 = AttrDict(vary=True, fiducial=0.45, prior='normal', mu=0.45, sigma=0.2)
     params.b2_01_a__1 = AttrDict(vary=True, fiducial=-1.5, prior='normal', mu=-1.5, sigma=0.5)
     params.b2_01_a__2 = AttrDict(vary=True, fiducial=0.7, prior='normal', mu=0.7, sigma=0.2)
-    params.b2_01_a    = AttrDict(vary=False, expr="[0., 0., b2_01_a__2, b2_01_a__1, b2_01_a__0]")
     
     params.options.append('b2_01_a')
 
@@ -147,7 +157,7 @@ def use_constrained_b2_01(params):
             params.options.pop(params.options.index('b2_01_a'))
         
     name = 'b2_01_b'
-    biases_params = [name] + [name + '__' + str(i) for i in [0, 1, 2]]
+    biases_params = [name + '__' + str(i) for i in [0, 1, 2]]
     
     # make the parameters valid
     for b in biases_params:
@@ -157,7 +167,7 @@ def use_constrained_b2_01(params):
     params.b2_01_b__0 = AttrDict(vary=True, fiducial=1.9, prior='normal', mu=1.9, sigma=0.6)
     params.b2_01_b__1 = AttrDict(vary=False, expr="b2_01_a__1")
     params.b2_01_b__2 = AttrDict(vary=False, expr="b2_01_a__2")
-    params.b2_01_b    = AttrDict(vary=False, expr="[0., 0., b2_01_b__2, b2_01_b__1, b2_01_b__0]")
+
     params.options.append('constrained_b2_01')
     
 def use_b2_01_b(params):
@@ -166,7 +176,7 @@ def use_b2_01_b(params):
     ``b2_01_b`` as parameters
     """
     name = 'b2_01_b'
-    biases_params = [name] + [name + '__' + str(i) for i in [0, 1, 2]]
+    biases_params = [name + '__' + str(i) for i in [0, 1, 2]]
     
     # make the parameters valid
     for b in biases_params:
@@ -175,7 +185,6 @@ def use_b2_01_b(params):
     params.b2_01_b__0 = AttrDict(vary=True, fiducial=1.9, prior='normal', mu=1.9, sigma=0.6)
     params.b2_01_b__1 = AttrDict(vary=True, fiducial=-1.9, prior='normal', mu=-1.9, sigma=0.6)
     params.b2_01_b__2 = AttrDict(vary=True, fiducial=0.9, prior='normal', mu=0.9, sigma=0.3)
-    params.b2_01_b    = AttrDict(vary=False, expr="[0., 0., b2_01_b__2, b2_01_b__1, b2_01_b__0]")
     
     params.options.append('b2_01_b')
     
