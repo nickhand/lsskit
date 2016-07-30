0aa892997000a36732451655418cc818397a003b
Nick Hand <nhand@berkeley.edu>
2016-07-30 14:40:58 -0700

    add a table header for nonlinear biases

--- lsskit/rsdfit/show/table.py
+++ lsskit/rsdfit/show/table.py
@@ -22,3 +22,16 @@ def fixed_alphas_header(model):
         return r"varying $\alpha_\perp$, $\alpha_\parallel$"
     else:
         return r"fixed $\alpha_\perp$, $\alpha_\parallel$"
+        
+def nonlinear_bias_header(model):
+    """
+    Comparison table headers for nonlinear bias model types
+    """
+    if model == 'b2_00_constrained_b2_01':
+        return r"$b_2^{00}$, constrained $b_2^{01}$"
+    elif model == 'constrained_b2_01':
+        return r"constrained $b_2^{01}$"
+    elif model == 'b2_01_a_b2_01_b':
+        return r"$b_{2,a}^{01}$, $b_{2,b}^{01}$"
+    else:
+        raise ValueError("`model` value '%s' not recognized" %model)
